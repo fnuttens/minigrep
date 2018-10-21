ce130c3c4c9b932466ecbbef75c38d56886a30cd
Florent Nuttens <fnuttens@gmail.com>
2018-10-21 22:43:49 +0200

    Read file content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +10,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    
+    println!("With text:\n{}", contents);
 }
